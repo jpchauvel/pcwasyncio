debbe19de1da8b88b1b3b26044724260ff8255dd
Jean-Pierre Chauvel <jean.p.chauvel@gmail.com>
2024-05-06 13:33:14 -0500

    bifurcation

--- blocking_server.py
+++ blocking_server.py
@@ -6,6 +6,7 @@ def main() -> None:
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     address: tuple[str, int] = ("127.0.0.1", 8000)
+    server_socket.setblocking(False)
     server_socket.bind(address)
     server_socket.listen()
 
